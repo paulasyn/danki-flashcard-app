b077eccdfdc267fd954631ece94ca5dc375e9970
Paul Asyn <pasyn3@gatech.edu>
2024-03-26 16:37:14 -0700

    formatting for 404 Not Found Page

--- src/Pages/NotFoundPage.tsx
+++ src/Pages/NotFoundPage.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 export default function NotFoundPage() {
   return (
     <div className="flex flex-col gap-2">
-        404 Not Found
+        <p>
+            404 Not Found
+        </p>
         <Link to='/'>Home</Link>
     </div>
   );
