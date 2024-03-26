c90e12f6abc2e03d37505af289621ff87d5a5392
Paul Asyn <pasyn3@gatech.edu>
2024-03-26 16:36:23 -0700

    added routing to individual decks for EditPage

--- src/Pages/EditDeck.tsx
+++ src/Pages/EditDeck.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Link } from 'react-router-dom';
 
 import '../index.css'
 
-
 export default function EditDeck() {
+  const decksList = [1, 2, 3];
+
+
   return (
-   <div><h1>Edit Deck</h1></div>
+    <div>
+    <h1>Edit Decks</h1>
+    <p></p>
+    <h2>Created Decks</h2>
+      <div className='flex flex-col gap-2'>
+        {decksList.map((deck) => (
+          <Link key={deck} to={`/edit/${deck}`}>
+            <p> 
+              Deck {deck} 
+            </p>
+          </Link>
+        ))}
+      </div>
+   </div>
   )
 }
 
