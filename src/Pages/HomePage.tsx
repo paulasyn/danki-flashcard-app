48aedbec95cf6f86b6b8a426ba51b889cfe1e034
Paul Asyn <pasyn3@gatech.edu>
2024-03-26 16:33:11 -0700

    added HomePage for main routing

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -2,11 +2,15 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import '../index.css'
+import { Link } from 'react-router-dom';
+
 
 
 export default function HomePage() {
   return (
-   <div><h1>Hello World!</h1></div>
+   <div className='flex flex-col gap-2'>
+    <h1>Home Page</h1>
+   </div>
   )
 }
 
