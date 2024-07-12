f03bab76296c70be66abee594e97f6532924ca67
Paul Asyn <paulasyn@gmail.com>
2024-07-12 12:17:27 -0700

    changed edit tab icon to pencil

--- app/(tabs)/edit.tsx
+++ app/(tabs)/edit.tsx
@@ -9,7 +9,7 @@ export default function EditScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="pencil-outline" style={styles.headerImage} />}>
+      headerImage={<Ionicons size={310} name="book" style={styles.headerImage} />}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Edit</ThemedText>
       </ThemedView>
